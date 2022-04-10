866c60e67445f696639041214f7d405c015d1936
trantiendat.711@gmail.com <trantiendat.711@gmail.com>
2022-04-10 10:33:20 +0700

    Setup S3 Storage for prod

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,11 +10,11 @@ module.exports = ({ env }) => ({
     upload: {
         provider: 'aws-s3',
         providerOptions: {
-            accessKeyId: env('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: env('AWS_ACCESS_SECRET'),
-            region: env('AWS_REGION'),
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_SECRET,
+            region: process.env.AWS_REGION,
             params: {
-                Bucket: env('AWS_BUCKET_NAME'),
+                Bucket: process.env.AWS_BUCKET_NAME,
             },
             logger: console // Only if you want to `stdout` logs
         }
--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,4 @@
 module.exports = ({ env }) => {
-  console.log('ENV: ', {
-    secretKey: env('AWS_ACCESS_KEY_ID'),
-    secretAccessKey: env('AWS_ACCESS_SECRET'),
-    region: env('AWS_REGION'),
-    params: {
-      Bucket: env('AWS_BUCKET_NAME'),
-    },
-  });
   return ({
     //
     graphql: {
@@ -16,17 +8,5 @@ module.exports = ({ env }) => {
         playgroundAlways: false,
       },
     },
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
-        },
-        logger: console // Only if you want to `stdout` logs
-      }
-    }
   })
 };
