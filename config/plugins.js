acd026e58a7f208fff99d3d63d727423574042fa
trantiendat.711@gmail.com <trantiendat.711@gmail.com>
2022-04-10 10:14:20 +0700

    Init aws s3 configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,13 @@
-module.exports = {
+module.exports = ({ env }) => {
+  console.log('ENV: ', {
+    secretKey: env('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: env('AWS_ACCESS_SECRET'),
+    region: env('AWS_REGION'),
+    params: {
+      Bucket: env('AWS_BUCKET_NAME'),
+    },
+  });
+  return ({
     //
     graphql: {
       config: {
@@ -7,5 +16,17 @@ module.exports = {
         playgroundAlways: false,
       },
     },
-  };
-   
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+        logger: console // Only if you want to `stdout` logs
+      }
+    }
+  })
+};
