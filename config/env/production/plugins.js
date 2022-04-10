52d2ed92697c22456eec2ae187785290a9623e3f
trantiendat.711@gmail.com <trantiendat.711@gmail.com>
2022-04-10 18:35:13 +0700

    Update S3 setup

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,15 +8,17 @@ module.exports = ({ env }) => ({
         },
     },
     upload: {
-        provider: 'aws-s3',
-        providerOptions: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_ACCESS_SECRET,
-            region: process.env.AWS_REGION,
-            params: {
-                Bucket: process.env.AWS_BUCKET_NAME,
-            },
-            logger: console // Only if you want to `stdout` logs
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+                region: env('AWS_REGION'),
+                params: {
+                    Bucket: env('AWS_BUCKET_NAME'),
+                },
+                logger: console // Only if you want to `stdout` logs
+            }
         }
     }
 });
--- config/plugins.js
+++ config/plugins.js
@@ -8,5 +8,20 @@ module.exports = ({ env }) => {
         playgroundAlways: false,
       },
     },
+    // upload: {
+    //   config: { 
+    //     provider: 'aws-s3',
+    //     providerOptions: {
+    //       accessKeyId: env('AWS_ACCESS_KEY_ID'),
+    //       secretAccessKey: env('AWS_ACCESS_SECRET'),
+    //       region: env('AWS_REGION'),
+    //       params: {
+    //         Bucket: env('AWS_BUCKET_NAME'),
+    //       },
+    //       logger: console // Only if you want to `stdout` logs
+    //     }
+    //   }
+    
+    // }
   })
 };
